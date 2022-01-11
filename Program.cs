171e6cb459e4203cc5e6f8ef0713a684066a6c8a
thomas <thomas.duditlieux@gmail.com>
2022-01-11 13:31:33 +0100

    push init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IpiPedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
